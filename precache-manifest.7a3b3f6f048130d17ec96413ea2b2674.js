c5f5f1db4f8dd24fc98757208dad7c97f0dd6a3e
Guilherme Nunes <guilhermesnd14@gmail.com>
2020-08-30 14:06:32 -0300

    Updates

---
+++ precache-manifest.7a3b3f6f048130d17ec96413ea2b2674.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "307b7b7b25225e99562dd5aef9c44fb3",
+    "url": "/micg-estimates/index.html"
+  },
+  {
+    "revision": "16e49187758bad7959e5",
+    "url": "/micg-estimates/static/js/2.5ab80732.chunk.js"
+  },
+  {
+    "revision": "1b9a420e3baf3e4789a942689afce96f",
+    "url": "/micg-estimates/static/js/2.5ab80732.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e48da5a58b0f8f40721",
+    "url": "/micg-estimates/static/js/main.0b4e7169.chunk.js"
+  },
+  {
+    "revision": "8bb62c85e5d2ef43590f",
+    "url": "/micg-estimates/static/js/runtime-main.f6d695bd.js"
+  },
+  {
+    "revision": "015352ba475be860d2c21eabeb459652",
+    "url": "/micg-estimates/static/media/add.015352ba.svg"
+  },
+  {
+    "revision": "a3b6918ad9b8bf7f41adfc95f35dff54",
+    "url": "/micg-estimates/static/media/arrow.a3b6918a.svg"
+  },
+  {
+    "revision": "41679b2630c50680c8bd13a375044821",
+    "url": "/micg-estimates/static/media/close.41679b26.svg"
+  }
+]);
