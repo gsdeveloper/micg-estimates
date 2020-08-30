d368eecbc8387f5cf9a070fb50cefe01c4ad34d2
Guilherme Nunes <guilhermesnd14@gmail.com>
2020-08-30 16:18:57 -0300

    Fix LOGO alignment

--- src/components/header/style.js
+++ src/components/header/style.js
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 
 export const HeaderStyled = styled.div`
   display: flex;
+  justify-content: center;
   align-items: center;
   height: 54px;
   background: #f2f5f7;
@@ -13,5 +14,6 @@ export const LogoStyled = styled.h4`
   font-size: 16px;
   line-height: 48px;
   color: #000000;
+  max-width: 1038px;
   padding: 0 8%;
 `;
