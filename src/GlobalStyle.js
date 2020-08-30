481ccb03404e9c137a13dc12034b13599492220e
Guilherme Nunes <guilhermesnd14@gmail.com>
2020-08-30 14:54:47 -0300

    Remove unused media queries breakpoints

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -1,26 +1,5 @@
 import { createGlobalStyle } from 'styled-components';
 
-const deviceSize = {
-  mobileS: '320px',
-  mobileM: '375px',
-  mobileL: '425px',
-  tablet: '768px',
-  laptop: '1024px',
-  laptopL: '1440px',
-  desktop: '2560px',
-};
-
-export const device = {
-  mobileS: `(min-width: ${deviceSize.mobileS})`,
-  mobileM: `(min-width: ${deviceSize.mobileM})`,
-  mobileL: `(min-width: ${deviceSize.mobileL})`,
-  tablet: `(min-width: ${deviceSize.tablet})`,
-  laptop: `(min-width: ${deviceSize.laptop})`,
-  laptopL: `(min-width: ${deviceSize.laptopL})`,
-  desktop: `(min-width: ${deviceSize.desktop})`,
-  desktopL: `(min-width: ${deviceSize.desktop})`,
-};
-
 const GlobalStyle = createGlobalStyle`
 
     #root, html, body {
@@ -44,7 +23,6 @@ const GlobalStyle = createGlobalStyle`
 
     body {
         box-sizing: inherit;
-        padding-bottom: 80px;
         @media (min-width: 768px) {
             background-color: #F8F8F8;
         }
